f7c6d9f28b8ffb6f9cded6092a0288d76c75ad17
FelixAshong <felixashong4@gmail.com>
2023-12-31 11:39:50 -0800

    INITIAL COMMIT

---
+++ footer.php
@@ -0,0 +1,17 @@
+  </section>
+    <!-- /.content -->
+    
+    <p></p>
+    Flowify Inc 
+    Copyright (C) 2023
+  </div>
+  <!-- /.content-wrapper -->
+
+  
+  
+</div>
+<!-- ./wrapper -->
+
+
+</body>
+</html>
